b194a1920fae911cb07e05bc3f9dfadae8970595
”tomofumi03” <kuchibuefuite@icloud.com>
2022-02-03 22:47:20 +0900

    add static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  def test
-    render html: "hello, world!"
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#test'
+  root 'static_pages#home'
+  get 'static_pages/home'
+
 end
