5fdda18c93a0cffce6f13f7becd2dc827bfbc3a2
”tomofumi03” <kuchibuefuite@icloud.com>
2022-02-17 21:42:30 +0900

    add passworddigest

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'bootsnap'
 gem 'bootstrap-sass', '3.3.7'
+gem 'bcrypt',         '3.1.12'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_051359) do
+ActiveRecord::Schema.define(version: 2022_02_17_120013) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
