4b996714d3aa76feabc7a4922afeaf63a214eb37
”tomofumi03” <kuchibuefuite@icloud.com>
2022-03-01 17:46:48 +0900

    add login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require rails-ujs
+//= require jquery3
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
-//= require jquery3
-//= require bootstrap-sprockets
+//= require_tree .
