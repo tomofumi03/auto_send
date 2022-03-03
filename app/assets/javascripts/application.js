6e1ab0f253ed63b35d06da3494bfa0a4f18c48b0
”tomofumi03” <kuchibuefuite@icloud.com>
2022-03-03 23:07:42 +0900

    change js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
-//= require bootstrap
-//= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require bootstrap
