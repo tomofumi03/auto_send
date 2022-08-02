9bbb9612fe2e408a42a33c3b7fafe39b45649732
”tomofumi03” <kuchibuefuite@icloud.com>
2022-08-02 21:01:01 +0900

    change content5

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -3,7 +3,6 @@ class CustomerMailer < ApplicationMailer
   def remind_mail(customer)
     @customer = customer
     mail to: @customer.email, subject: '[自動送信]支払い期限が迫っています'
-    format.html #HTMLメールを指定
   end
 
 end
