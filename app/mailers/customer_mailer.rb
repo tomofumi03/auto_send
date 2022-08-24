6d6fc6cd2690107470db0e48d34c467433093324
”tomofumi03” <kuchibuefuite@icloud.com>
2022-08-24 22:05:25 +0900

    add if sentence in everyonemailer

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -2,7 +2,11 @@ class CustomerMailer < ApplicationMailer
 
   def remind_mail(customer)
     @customer = customer
-    mail to: @customer.email, subject: @customer.subject
+    if @customer.subject == nil
+      mail to: @customer.email, subject: "お知らせ"
+    else
+      mail to: @customer.email, subject: @customer.subject
+    end
   end
 
 end
--- app/mailers/everyone_mailer.rb
+++ app/mailers/everyone_mailer.rb
@@ -3,7 +3,11 @@ class EveryoneMailer < ApplicationMailer
 
   def remind_mail_to_everyone(customers)
     customers.each do |customer|
-      mail bcc: customer[0], subject: customer[1]
+      if customer[1] == nil
+        mail bcc: customer[0], subject: "お知らせ"
+      else
+        mail bcc: customer[0], subject: customer[1]
+      end
     end
   end
 
