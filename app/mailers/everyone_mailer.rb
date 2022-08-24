f844e6f51d5a61aadaa2203f35b7935ef73b800e
”tomofumi03” <kuchibuefuite@icloud.com>
2022-08-24 17:15:56 +0900

    add repeat to everyone mailer

--- app/mailers/everyone_mailer.rb
+++ app/mailers/everyone_mailer.rb
@@ -2,7 +2,9 @@ class EveryoneMailer < ApplicationMailer
 
 
   def remind_mail_to_everyone(customers)
-      mail bcc: customers[0], subject: [1]
+    customers.each do |customer|
+      mail bcc: customer[0], subject: customer[1]
+    end
   end
 
 end
